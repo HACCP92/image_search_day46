838453bea07b240b604e90ea49cbe1bb3703b8d3
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-11-11 15:59:39 +0900

    [이미지 검색앱] detail페이지 go_router 작성

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_search_day46/data/repository/pixabay_photo_repository.dart';
+import 'package:image_search_day46/domain/model/photo.dart';
 import 'package:image_search_day46/domain/use_case/get_top_five_most_viewd_use_case.dart';
+import 'package:image_search_day46/presentation/detail/detail_screen.dart';
+import 'package:image_search_day46/presentation/main/main_screen.dart';
 import 'package:image_search_day46/presentation/main/main_view_model.dart';
 import 'package:provider/provider.dart';
-import '../../presentation/main/main_screen.dart';
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
@@ -20,6 +22,15 @@ final GoRouter router = GoRouter(
           child: const MainScreen(),
         );
       },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'details',
+          builder: (BuildContext context, GoRouterState state) {
+            final photo = state.extra as Photo;
+            return DetailScreen(photo: photo);
+          },
+        ),
+      ],
     ),
   ],
 );
